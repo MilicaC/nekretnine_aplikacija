00a2f7424996b9bc4d74c87faa8767a24083a021
Milica Cakarevic <mc20170147@student.fon.bg.ac.rs>
2021-06-09 21:24:34 +0200

    register i login

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
-
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import {FormBuilder} from '@angular/forms';
 
 import {AuthService} from '../auth.service';
 
@@ -14,31 +16,76 @@ import {AuthService} from '../auth.service';
 })
 export class RegisterPage implements OnInit {
 
+ 
+
+
+
    registerForm: FormGroup;
-  constructor(private authService: AuthService,private http: HttpClient) { }
+
+  
+
+
+
+
+
+  constructor(private authService: AuthService,private http: HttpClient, private router: Router,
+    private loadingCtrl: LoadingController, public toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.registerForm= new FormGroup({
-      Ime: new FormControl('name'),
-      Prezime: new  FormControl(null),
-      Email: new FormControl(null),
-      Lozinka: new FormControl(null),
-      Ponovilozinku: new FormControl(null)
+     // Ime: new FormControl(null),
+      // ovde je imalo validatora u svakom redu
+      //Prezime: new  FormControl(null),
+      //Email: new FormControl(null),
+      //Lozinka: new FormControl(null),
+      //Ponovilozinku: new FormControl(null)
+       Ime: new FormControl(null, Validators.required),
+      Prezime: new FormControl(null, Validators.required),
+      Email: new FormControl(null, [Validators.required, Validators.email]),
+      Lozinka: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      Ponovilozinku: new FormControl(null, [Validators.required, Validators.minLength(8)])
     });
+    
   }
     
  
-   addAuthor(){
-      if( this.registerForm.value['Lozinka']== this.registerForm.value['Ponovilozinku']){
-      this.authService.addAuthor(this.registerForm.value['Ime'],
-      this.registerForm.value['Prezime'],
-      this.registerForm.value['Email'],
-      this.registerForm.value['Lozinka'],
-      this.registerForm.value['Ponovilozinku']).subscribe(res=>console.log(res));
+   async addAuthor(){
+     if(this.registerForm.get('Ime').invalid || this.registerForm.get('Prezime').invalid || this.registerForm.get('Email').invalid ||
+     this.registerForm.get('Lozinka').invalid || this.registerForm.get('Ponovilozinku').invalid ){
+       const toast= this.toastCtrl.create({
+         message: "Moraju biti popunjena sva polja.",
+         duration: 3000
+       });
+       (await toast).present();
+       this.registerForm.reset();
+      }else if(this.registerForm.get('Lozinka') != this.registerForm.get('Ponovilozinku')){
+        const toast= this.toastCtrl.create({
+          message: "Ne poklapaju se unete lozinke.",
+          duration: 3000
+        });
+        (await toast).present();
+        this.registerForm.reset();
       }
-      else console.log("Neuspela registracija")
-   
+     else{
+       this.loadingCtrl.create({message: "Registering..."}).then((loadingEl:HTMLIonLoadingElement)=>{
+     loadingEl.present();
+        this.authService.addAuthor(this.registerForm.value).subscribe(resData=>{
+          console.log('Registracija je uspela');
+          console.log(resData);
+          loadingEl.dismiss();
+          this.registerForm.reset();
+          this.router.navigateByUrl('/log-in');
+        
+        });
+      
+          
+
+        });
+      
+      }
 }
+
+// ova metoda izgleda ne radi nista
   register() {
       // @ts-ignore
     this.authService.register().subscribe(resData => {
